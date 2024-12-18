23ba5813a2aabda279d18eaa6c32e2c12ed80740
Mercan Esen <mesen186@gmail.com>
2024-12-18 20:43:06 +0300

    Add project files.

--- API_WeatherConsume/Program.cs
+++ API_WeatherConsume/Program.cs
@@ -101,4 +101,86 @@ if (number == "3")
 
 
 }
+if(number == "4")
+{
+    string url = "http://localhost:5242/api/WeathersControllers?id=";
+    Console.WriteLine("*** Silmek İstediğiniz Şehrin Id sini Giriniz:");
+    int id = int.Parse(Console.ReadLine());
+
+    using (HttpClient client = new HttpClient())
+    {
+        HttpResponseMessage response = await client.DeleteAsync(url + id);
+        response.EnsureSuccessStatusCode();
+    }
+
+
+}
+if(number== "5")
+{
+    Console.Write("*** Veri Güncelleme İşlemi ***\n");
+    string cityName, country, detail;
+    decimal temp;
+    int id;
+
+    Console.Write("Şehir Id:");
+    id=int.Parse(Console.ReadLine());
+
+    Console.Write("Şehir Adı:");
+    cityName = Console.ReadLine();
+
+    Console.Write("Ülke Adı:");
+    country = Console.ReadLine();
+
+    Console.Write("Sıcaklık:");
+    temp = decimal.Parse(Console.ReadLine());
+
+    Console.Write("Hava Durumu Bilgisi:");
+    detail = Console.ReadLine();
+    string url = "http://localhost:5242/api/WeathersControllers";
+    var updateWeather = new 
+    {       CityId=id,
+            CityName = cityName,
+            Country = country,
+            Temp = temp,
+            Detail = detail
+
+};
+    using(HttpClient client = new HttpClient())
+    {
+        string json = JsonConvert.SerializeObject(updateWeather);
+        StringContent content = new StringContent(json , Encoding.UTF8 , "application/json");
+        HttpResponseMessage response = await client.PutAsync(url, content);
+        response.EnsureSuccessStatusCode();
+
+    }  
+}
+if(number== "6")
+{
+    string url = "http://localhost:5242/api/WeathersControllers/GetIdWeatherCity?id=";
+
+    Console.Write("Bilgilerinizi Getirmek İstediğiniz Şehrin Id sini Giriniz:");
+    int id = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    using(HttpClient client = new HttpClient())
+    {
+
+        HttpResponseMessage response = await client.GetAsync(url + id);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        JObject weatherCityObject = JObject.Parse(responseBody);
+
+        string cityName = weatherCityObject["cityName"].ToString();
+        string detail = weatherCityObject["detail"].ToString();
+        string country = weatherCityObject["country"].ToString();
+        decimal temp =decimal.Parse(weatherCityObject["temp"].ToString());
+
+        Console.WriteLine("Girmiş Olduğunuz Id ye Göre Bilgiler");
+        Console.WriteLine();
+        Console.Write("Şehir: " + cityName + "\nÜlke: " + country + "\nDetay: " + detail + "\nSıcaklık: " + temp);
+
+
+
+    }
+}
 Console.Read();
